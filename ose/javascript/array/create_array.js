e066c43079421df568460a7aec61122186b874f2
Sandip <codewithsandip@gmail.com>
2022-09-19 18:07:30 +0530

    js array

---
+++ ose/javascript/array/create_array.js
@@ -0,0 +1,11 @@
+//An empty array:
+let todoList = [];
+
+//Array of strings:
+let shoppingList = [ 'cereal', 'cheese', 'ice' ];
+
+// Array of numbers:
+let lotto = [ 45, 12, 23, 25, 34 ];
+
+// Array of multiple types:
+let myCollection = [ 12, 'dog', true, null, NaN ];
