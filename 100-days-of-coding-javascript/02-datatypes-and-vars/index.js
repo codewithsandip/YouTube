82b84d0fe93e647577b6e43423644740c36193cd
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-03-31 17:18:26 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/02-datatypes-and-vars/index.js
@@ -0,0 +1,49 @@
+// this is one line comment
+
+/*
+* this is multiline comment
+*/
+
+/*
+// iphone data
+"9:41" // time - string
+"Wed, Aug 14" // date - string
+"Armando" // name - string
+3 // message count - number
+
+// product data
+"https://amazon.com/iphone.jpg" // product image url - string
+"Apple iPhone 14 Pro Max" // product name - string
+149900 // product price - number
+
+// spotify form data
+"sandip@gmail.com" // email - string
+"secret" // password - string
+true // allowShareMyData - boolean */
+
+
+// define a variable
+//let time;
+//time = "9:41";
+let time = "9:41";
+
+console.log(time);
+
+time = "12:00";
+
+console.log(time);
+
+// camel case or snake case
+let productImageUrl = "https://amazon.com/iphone.jpg";
+
+let productName = "Apple iPhone 14 Pro Max";
+
+let messageCount = 3;
+
+let allowShareMyData = false;
+
+// two different variables
+let age = 20;
+let Age = 50;
+
+let let1 = 300;
