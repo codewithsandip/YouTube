dd8765f8d8a161bd6aa9a17cc9b71a30bd8b56d9
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:25:41 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/06-null-undefined/index.js
@@ -0,0 +1,13 @@
+let user = null;
+// user has logged in
+user = "Sandip";
+console.log(user);
+// user has logged out
+user = null;
+console.log(user);
+
+let username;
+console.log(username); // undefined
+
+console.log("Hello " + user);
+console.log("Hello " + username);
