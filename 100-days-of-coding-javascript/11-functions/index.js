5537a4048703ac427ac38345c0076033f1463096
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:26:54 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/11-functions/index.js
@@ -0,0 +1,24 @@
+// function greet() {
+//     console.log("Hello Sandip!");
+// }
+
+// greet();
+
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+
+// greet("Jimmy");
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// sum(10, 30);
+
+function sum(a, b) {
+    return a + b;
+}
+
+const result = sum(30, 12);
+console.log(result);
