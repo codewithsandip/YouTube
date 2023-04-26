d6ffd3a9f33f8ecabc47db363d84f78b22580609
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:25:27 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/05-booleans/index.js
@@ -0,0 +1,19 @@
+let openForWork = true;
+let married = false;
+let gameOver = "true"; // string
+let yes = "yes"; // string
+let no = "no"; // string
+
+console.log("She was " + true); // She was true
+console.log(true + " is only TRUE"); // true is only TRUE
+console.log("In between " + true + false + " I feel lost!");
+// In between truefalse I feel lost!
+console.log(true + false + " can be TRUE or FALSE");
+// 1 can be TRUE or FALSE
+console.log("Only " + (true + false) + " this can be TRUE!");
+// Only 1 this can be TRUE!
+
+console.log(true - false); // 1-0 = 1
+console.log(true * false); // 1 * 0 = 0
+console.log(true / false); // 1 / 0 = Infinity
+console.log(true % false); // 1 % 0 = NaN
