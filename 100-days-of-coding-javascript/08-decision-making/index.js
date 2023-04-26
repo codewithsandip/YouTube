28d838b0f887029ddf48a1c2c05bc56b9b967f09
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:26:12 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/08-decision-making/index.js
@@ -0,0 +1,38 @@
+/*let age = 15;
+
+if (age > 18) {
+    console.log("You are an adult!");
+}*/
+
+/*let age = 15;
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are not an adult yet.");
+}*/
+
+/*let score = 69;
+
+if (score >= 90) {
+    console.log("You got an A!");
+} else if (score >= 80) {
+    console.log("You got a B!");
+} else if (score >= 70) {
+    console.log("You got a C.");
+} else {
+    console.log("You did not pass.");
+}*/
+
+let age = 15;
+let gender = "female";
+
+if (age >= 18) {
+    if (gender === "male") {
+        console.log("You are a man.");
+    } else {
+        console.log("You are a woman.");
+    }
+} else {
+    console.log("You are not an adult yet.");
+}
