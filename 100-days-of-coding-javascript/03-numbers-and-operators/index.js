de22e9bbe00efb210e2b1df7106c4c59bf50c3d6
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:24:55 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/03-numbers-and-operators/index.js
@@ -0,0 +1,54 @@
+// numbers
+let age = 30;
+let loss = -5000;
+let numOfStarsInGalaxy = 200000000;
+let distance = 15.5;
+let pi = 3.14000000000000289;
+console.log(pi); // 3.140000000000003
+
+// math operators
+let sum = 50 + 5;
+console.log(sum);
+let counter = 10 - 1;
+console.log(counter);
+let rates = 5000 * 0.5;
+console.log(rates);
+let pieces = 100 / 4;
+console.log(pieces);
+let remaining = 35 % 2; // modulus
+console.log(remaining);
+let log = 3 ** 2; // 3*3
+console.log(log);
+
+// operator precedence
+//let result = 5 + 5 / 2; // 5
+// console.log(result);
+let result = (5 + 5) / 2;
+console.log(result);
+
+// NaN & Infinity
+let out = 0 / 0;
+console.log(out); // NaN
+let a = 1 + NaN;
+console.log(a);
+let b = 1 / 0;
+console.log(b); // Infinity
+
+// unary operators
+let score = 0;
+score = score + 10;
+console.log(score);
+score++;
+console.log(score);
+score--;
+console.log(score);
+score += 4;
+console.log(score);
+score -= 4;
+console.log(score);
+
+let money = 100;
+money *= 10;
+console.log(money);
+money /= 2;
+console.log(money);
