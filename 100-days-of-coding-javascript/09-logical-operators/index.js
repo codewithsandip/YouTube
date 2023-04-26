42c9a016d669ba94c8706b01c58e7ac593f57841
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:26:25 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/09-logical-operators/index.js
@@ -0,0 +1,25 @@
+/*let age = 16;
+let hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+    console.log("You can legally drive.");
+} else {
+    console.log("You cannot legally drive.");
+}*/
+
+/*let age = 15;
+let hasPermission = false;
+
+if (age >= 18 || hasPermission) { 
+    console.log("You can watch the movie.");
+} else {
+    console.log("You cannot watch the movie.");
+}*/
+
+let isRaining = true;
+
+if (!isRaining) {
+    console.log("It is not raining.");
+} else {
+    console.log("It is raining.");
+}
