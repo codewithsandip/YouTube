89ee69a695ea4a4388effef930e692e197bff7d7
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:25:09 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/04-strings/index.js
@@ -0,0 +1,27 @@
+let name = "Sandip";
+let profession = 'Software Developer';
+let objective = "A highly motivated and experienced technical lead seeking a challenging position in a fast-paced and dynamic organisation. Bringing a strong background in project management, technology, and leadership to drive teams to achieve goals and deliver results."
+//let error = "This wont work!';
+let salary = "20000"; // string
+let special = "121teets*&^%)(-+nsdkf=";
+let message = "I don't like that";
+let anotherMessage = 'It"s really miserable!';
+
+let firstName = "Sonali";
+let lastName = "Kulkarni";
+console.log(firstName + lastName);
+console.log(firstName + " " + lastName);
+console.log(firstName + " " + lastName + " is good in singing");
+console.log("Hi" - "Bye"); // NaN
+console.log("Hi" * "Bye"); // NaN
+console.log("Hi" / "Bye"); // NaN
+console.log("Hi" % "Bye"); // NaN
+
+console.log("Sonali is " + 2 + 7 + " years old"); // 27
+console.log(2 + 7 + " years ago Sonali was born"); // 9
+console.log("Sonali is " + (2 + 7) + " years old"); // 9
+
+console.log("Sonali is " + 2 - 7 + " years old"); // NaN years old
+console.log("Sonali is " + 2 * 7 + " years old"); // 14
+console.log("Sonali is " + 30 / 5 + " years old"); // 6
+console.log("Sonali is " + 37 % 5 + " years old"); // 2
