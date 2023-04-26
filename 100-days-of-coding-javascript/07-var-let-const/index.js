6bdd4ba664d1c882320e821d8c6a811b3d0b6017
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:25:55 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/07-var-let-const/index.js
@@ -0,0 +1,9 @@
+var age = 30;
+
+let salary = 20000;
+salary = 50000;
+console.log(salary);
+
+const pastMemory = "I was happy when I was a kid";
+//pastMemory = "I want to be kid again";
+console.log(pastMemory);
