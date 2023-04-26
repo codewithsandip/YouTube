95e3f513e692196c0eaa834c819d0a4ed39993fa
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:26:42 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/10-compariosn-operators/index.js
@@ -0,0 +1,11 @@
+/*console.log(5 == "5"); // true, because the string "5" is converted to the number 5
+console.log(true == 1); // true, because the boolean true is converted to the number 1
+console.log(null == undefined); // true, because both have the same value of "null"
+console.log(0 == false); // true, because the boolean false is converted to the number 0*/
+
+
+console.log(5 === "5"); // false, because the data types are different (number vs string)
+console.log(true === 1); // false, because the data types are different (boolean vs number)
+console.log(null === undefined); // false, because the data types are different (object vs undefined)
+console.log(0 === false); // false, because the data types are different (number vs boolean)
+
