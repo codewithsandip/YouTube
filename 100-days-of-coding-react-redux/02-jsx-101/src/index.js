c8f29cbb3ee9cb0c65a773c292fa1324ad62bcd6
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-04-26 12:19:20 +0530

    Add files via upload

---
+++ 100-days-of-coding-react-redux/02-jsx-101/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el);
+
+root.render(
+    <div>
+        <h2>My interesting title</h2>
+        <p>some random text ansdajsfsdjsd</p>
+    </div>
+);
