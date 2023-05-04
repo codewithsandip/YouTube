e6c452f0b5b84d8a91837c74d195231402d14254
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-05-04 12:26:43 +0530

    Add files via upload

---
+++ 100-days-of-coding-react-redux/18-notes-app-api-integration/src/components/App.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import AddNote from "./AddNote";
+import Header from "./Header";
+import NoteList from "./NoteList";
+
+import './App.css';
+
+function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        getAllNotes();
+    }, []);
+
+    const getAllNotes = async () => {
+        const response = await axios.get(`http://localhost:3005/notes`);
+        setNotes(response.data);
+    }
+
+    const createNote = async note => {
+        const response = await axios.post(`http://localhost:3005/notes`, note);
+        setNotes([...notes, response.data]);
+    }
+
+    const updateNote = editNote => {
+        const state = notes.map((note) => {
+            if (note.id === editNote.id) {
+                return { ...editNote };
+            }
+            return note;
+        });
+
+        setNotes([...state]);
+    }
+
+    const deleteNote = id => {
+        const state = notes.filter(note => note.id !== id);
+        setNotes([...state]);
+    }
+
+    return <div>
+        <div className="container-fluid shadow">
+            <div className="container">
+                <Header />
+            </div>
+        </div>
+        <div className="container-fluid">
+            <div className="container section">
+                <NoteList notes={notes}
+                    onEditNote={updateNote}
+                    onDeleteNote={deleteNote} />
+            </div>
+        </div>
+        <div className="container-fluid">
+            <div className="container section">
+                <AddNote onAddNote={createNote} />
+            </div>
+        </div>
+    </div>
+}
+
+export default App;
