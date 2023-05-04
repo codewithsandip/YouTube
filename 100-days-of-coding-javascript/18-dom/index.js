54e3d84351d69cf0b7db455c3cb5e284cdbdeec6
codewithsandip <70803038+codewithsandip@users.noreply.github.com>
2023-05-04 14:25:40 +0530

    Add files via upload

---
+++ 100-days-of-coding-javascript/18-dom/index.js
@@ -0,0 +1,6 @@
+const passwordInput = document.querySelector(".password-input");
+const lengthInput = document.querySelector(".length-input");
+const numbersCheckbox = document.querySelector(".number-input");
+const symbolCheckbox = document.querySelector("symbols-input");
+const generateBtn = document.querySelector(".generate-button");
+const copyBtn = document.querySelector(".copy-button");
